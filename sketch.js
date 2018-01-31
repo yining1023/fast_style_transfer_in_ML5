e4d53b093b9217dd554b269cb9cb6e3ff41ed493
Yining Shi <ys2189@nyu.edu>
2018-01-31 13:42:27 -0500

    switch to p5.array3d2image

--- sketch.js
+++ sketch.js
@@ -9,25 +9,25 @@ let modelNames = ['la_muse', 'rain_princess', 'udnie', 'wreck', 'scream', 'wave'
 let inputImg, styleImg;
 let outputImgData;
 let outputImg;
-let isLoading = true;
 let modelNum = 0;
 let currentModel = 'wave';
 let uploader;
+let webcam = false;
 
 function setup() {
-  createCanvas(252, 252).parent('canvasContainer');;
+  noCanvas();
   inputImg = select('#input-img').elt;
   styleImg = select('#style-img').elt;
   modelNames.forEach(n => {
     nets[n] = new p5ml.TransformNet('models/' + n + '/', modelLoaded);
   });
-  textSize(32);
-  textAlign(CENTER);
-  fill('#767676');
 
   // Image uploader
   uploader = select('#uploader').elt;
   uploader.addEventListener('change', gotNewInputImg);
+
+  outputImgContainer = createImg('images/wave.jpg', 'image');
+  outputImgContainer.parent('output-img-container');
 }
 
 // A function to be called when the model has been loaded
@@ -39,48 +39,37 @@ function modelLoaded() {
 }
 
 function predictImg(modelName) {
-  /**
-  * @param inputImg HTMLImageElement of input img
-  * @return Array3D containing pixels of output img
-  */
   outputImgData = nets[modelName].predict(inputImg);
-  // Convert the Array3D with image data to a p5.Image
-  outputImg = array3DToP5Image(outputImgData);
-  isLoading = false;
-  // Draw the p5.Image on the canvas
-  image(outputImg, 0, 0);
+  outputImg = p5ml.array3DToImage(outputImgData);
+  outputImgContainer.elt.src = outputImg.src;
 }
 
-function draw() {
-  if (isLoading) {
-    text('Loading...', width / 2, height / 2)
-  }
-}
+function draw() {}
 
 /**
 * @param imgData Array3D containing pixels of a img
 * @return p5 Image
 */
-function array3DToP5Image(imgData) {  
-  const imgWidth = imgData.shape[0];
-  const imgHeight = imgData.shape[1];
-  const data = imgData.dataSync();
-  const outputImg = createImage(imgWidth, imgHeight);
-  outputImg.loadPixels();
-  let k = 0;
-  for (let i = 0; i < outputImg.width; i++) {
-    for (let j = 0; j < outputImg.height; j++) {
-      k = (i + j * height) * 3;
-      let r = floor(256 * data[k + 0]);
-      let g = floor(256 * data[k + 1]);
-      let b = floor(256 * data[k + 2]);
-      let c = color(r, g, b);
-      outputImg.set(i, j, c);
-    }
-  }
-  outputImg.updatePixels();
-  return outputImg;
-}
+// function array3DToP5Image(imgData) {  
+//   const imgWidth = imgData.shape[0];
+//   const imgHeight = imgData.shape[1];
+//   const data = imgData.dataSync();
+//   const outputImg = createImage(imgWidth, imgHeight);
+//   outputImg.loadPixels();
+//   let k = 0;
+//   for (let i = 0; i < outputImg.width; i++) {
+//     for (let j = 0; j < outputImg.height; j++) {
+//       k = (i + j * height) * 3;
+//       let r = floor(256 * data[k + 0]);
+//       let g = floor(256 * data[k + 1]);
+//       let b = floor(256 * data[k + 2]);
+//       let c = color(r, g, b);
+//       outputImg.set(i, j, c);
+//     }
+//   }
+//   outputImg.updatePixels();
+//   return outputImg;
+// }
 
 function updateStyleImg(ele) {
   if (ele.src) {
@@ -88,14 +77,12 @@ function updateStyleImg(ele) {
     currentModel = ele.id;
   }
   if (currentModel) {
-    isLoading = true;
     predictImg(currentModel);
   }
 }
 
 function updateInputImg(ele) {
   if (ele.src) inputImg.src = ele.src;
-  isLoading = true;
   predictImg(currentModel);
 }
 
@@ -110,6 +97,10 @@ function gotNewInputImg() {
     inputImg.style.width = '250px';
     inputImg.style.height = '250px';
   }
+}
+
+function useWebcam() {
+  webcam = true;
 }
 
 function onPredictClick() {
